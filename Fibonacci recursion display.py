6e29c50a7b76777470ca14a9620b3454ccb1b4ae
Omer Friedman <omer100f@gmail.com>
2021-08-06 08:07:18 +0300

    Update and rename Fibonacci recursion display to Fibonacci recursion display.py

--- Fibonacci recursion display.py
+++ Fibonacci recursion display.py
@@ -4,10 +4,9 @@ def Fibo(a):
         Fibo(a - 1)
         print(Fibo(a * -1))
     elif(a < 0):
-        #Secondery loop for a specific place
+        #Secondery loop for a each place
         if(a >= -2):
             #First two places
             return 1
         return Fibo(a + 1) + Fibo(a + 2)
-
-Fibo(24) 
+""" For example: "Fibo(5)" will give the output of: "1 1 2 3 5" """
